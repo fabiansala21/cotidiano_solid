1f785969f8eb27734172b22ca9a68eba57c9be63
fabiansala21 <fabiansala0821@gmail.com>
2025-04-01 17:27:14 +0200

    lsp

---
+++ src/LSP/Ejercicio_1_sin.kt
@@ -0,0 +1,17 @@
+package LSP
+/*
+open class Cuadrado(var lado: Int) {
+    open fun calcularArea(): Int {
+        return lado * lado
+    }
+}
+class Rectangulo(lado: Int, var altura: Int) : Cuadrado(lado) {
+    override fun calcularArea(): Int {
+        return lado * altura
+    }
+}
+fun main() {
+    val cuadrado: Cuadrado = Rectangulo(5, 10)
+
+
+ */
